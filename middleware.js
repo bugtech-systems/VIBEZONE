e304fb4b0ef87afd79db201e3e375932d14d8357
I am root <jaybee.aicpa@gmail.com>
2024-10-17 17:21:02 +0800

    2nd commit

--- middleware.js
+++ middleware.js
@@ -8,7 +8,6 @@ export default authMiddleware({
 });
  
 export const config = {
-  runtime: 'nodejs', // Switch to Node.js runtime
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
